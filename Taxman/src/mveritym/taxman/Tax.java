24458c0d3626b4a5312e742dfe6048b1af0cef69
Melissa Marshall <mmarsha2@macalester.edu>
2011-08-28 06:25:51 -0500

    General setup, added Tax class.

---
+++ Taxman/src/mveritym/taxman/Tax.java
@@ -0,0 +1,5 @@
+package mveritym.taxman;
+
+public class Tax {
+
+}
--- Taxman/src/mveritym/taxman/Taxman.java
+++ Taxman/src/mveritym/taxman/Taxman.java
@@ -5,22 +5,28 @@ import java.util.logging.Logger;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.util.config.Configuration;
-//import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Taxman extends JavaPlugin{
-	
-	public Logger log = Logger.getLogger("Minecraft");
+
 	public Configuration config;
+	public Logger log = Logger.getLogger("Minecraft");
+	public PluginDescriptionFile info = null;
+	public PluginManager pluginManager = null;
 	
-	public void onEnable() {
-		log.info("Taxman plugin has been enabled.");
+	public void onEnable() {		
 		config = getConfiguration();
 		config.save();
+		info = getDescription();
+		pluginManager = getServer().getPluginManager();
+		
+		log.info(info.getName() + " " + info.getVersion() + " has been enabled.");
 	}
 	
 	public void onDisable() {
-		log.info("Taxman plugin has been disabled.");		
+		log.info(info.getName() + " " + info.getVersion() + " has been disabled.");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
