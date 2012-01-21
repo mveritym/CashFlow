f865646ab27e9eeb221da7bed0dfa98c25390ee6
Mitsugaru <mitsugaru@gmail.com>
2012-01-21 17:34:36 -0500

    Redid logic and unit conversion of CatchUp. In theory, the time persistence works. Test ran fairly close to expected, except for delay... Delay might need to be recalculated properly for when there are extra iterations.

--- Taxman/src/mveritym/cashflow/CashFlow.java
+++ Taxman/src/mveritym/cashflow/CashFlow.java
@@ -54,7 +54,7 @@ public class CashFlow extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		//Check for updates to database:
+		// Check for updates to database:
 		config.checkUpdate();
 		pluginManager = getServer().getPluginManager();
 
@@ -62,24 +62,26 @@ public class CashFlow extends JavaPlugin {
 		Listener listener = new Listener(this);
 		pluginManager.registerEvent(Event.Type.PLAYER_JOIN, listener,
 				Event.Priority.Monitor, this);
-		//Grab Permissions
+		// Grab Permissions
 		permsManager = new PermissionsManager(this);
 		// Grab Economy
 		this.setupEconomy();
 
-		//Create tax/salary managers
+		// Create tax/salary managers
 		taxManager = new TaxManager(this);
 		salaryManager = new SalaryManager(this);
 
-		//Instantiate Buffer
+		// Instantiate Buffer
 		Buffer buffer = Buffer.getInstance();
 		buffer.setup(this, taxManager, salaryManager);
 		buffer.start();
 
 		// Set up command executors
-		CashFlowCommand cashFlowCom = new CashFlowCommand(this, permsManager, taxManager, salaryManager);
+		CashFlowCommand cashFlowCom = new CashFlowCommand(this, permsManager,
+				taxManager, salaryManager);
 		TaxCommand taxCom = new TaxCommand(this, permsManager, taxManager);
-		SalaryCommand salaryCom = new SalaryCommand(this, permsManager, salaryManager);
+		SalaryCommand salaryCom = new SalaryCommand(this, permsManager,
+				salaryManager);
 		getCommand("cashflow").setExecutor(cashFlowCom);
 		getCommand("tax").setExecutor(taxCom);
 		getCommand("salary").setExecutor(salaryCom);
@@ -89,10 +91,17 @@ public class CashFlow extends JavaPlugin {
 		// Enable taxes/salaries
 		taxManager.enable();
 		salaryManager.enable();
-		//Check the last paid and see how many times to iterate the tax/salary
+		// Check the last paid and see how many times to iterate the tax/salary
 		final int tickToHour = 72000;
-		int id = this.getServer().getScheduler().scheduleSyncDelayedTask(this, new CatchUp(log, config, taxManager, salaryManager, prefix), (long) (config.catchUpDelay * tickToHour));
-		if(id == -1)
+		int id = this
+				.getServer()
+				.getScheduler()
+				.scheduleSyncDelayedTask(
+						this,
+						new CatchUp(log, config, taxManager, salaryManager,
+								prefix),
+						(long) (config.catchUpDelay * tickToHour));
+		if (id == -1)
 		{
 			this.log.severe("Could not schedule the CatchUp thread...");
 		}
@@ -104,7 +113,7 @@ public class CashFlow extends JavaPlugin {
 		this.saveConfig();
 		// Disable taxes/salaries and finish the buffers if any exist
 		// thus no economy changes are lost
-		if(economyFound)
+		if (economyFound)
 		{
 			taxManager.disable();
 			salaryManager.disable();
@@ -147,81 +156,90 @@ public class CashFlow extends JavaPlugin {
 		return database;
 	}
 
-	static class CatchUp implements Runnable
-	{
+	static class CatchUp implements Runnable {
 		private Logger log;
 		private Config config;
 		private TaxManager taxManager;
 		private SalaryManager salaryManager;
 		private String prefix;
 
-		public CatchUp(Logger l, Config conf, TaxManager tax, SalaryManager sal, String p)
-		{
+		public CatchUp(Logger l, Config conf, TaxManager tax,
+				SalaryManager sal, String p) {
 			log = l;
 			config = conf;
 			taxManager = tax;
 			salaryManager = sal;
 			prefix = p;
 		}
+
 		@Override
 		public void run() {
-			//Grab current time
+			// Grab current time
+			log.info(prefix + " Running CatchUp");
 			final long currentTime = System.currentTimeMillis();
-			//Unit conversion
+			// Unit conversion
 			final double millisecondToSecond = 0.001;
 			final long hoursToSeconds = 3600;
 			final int tickToHour = 72000;
-			//Grab all enabled taxes and check their time
-			for(Taxer tax : taxManager.taxTasks)
+			// Grab all enabled taxes and check their time
+			for (Taxer tax : taxManager.taxTasks)
 			{
 				final String name = tax.getName();
-				final long past = config.getLong("taxes." + name + ".lastpaid");
-				double hoursDiff = ((currentTime - past)*millisecondToSecond)/hoursToSeconds;
-				final double interval = config.getDouble(("taxes." + name + ".taxInterval"), 1);
-				if(hoursDiff > interval)
+				final long past = config.getLong("taxes." + name + ".lastPaid");
+				if (past > 0)
 				{
-					//The difference in hours is greater than the interval
-					//Do the number of iterations of the specified tax
-					final double iterations = hoursDiff / interval;
-					for(int i = 0; i < iterations; i++)
+					double hoursDiff = (((currentTime - past) * millisecondToSecond) / hoursToSeconds);
+					final double interval = config.getDouble(
+							("taxes." + name + ".taxInterval"), 1);
+					final long period = Math.round(interval * tickToHour) + 1;
+					long delay = (long) ((interval * tickToHour) - (config.catchUpDelay * tickToHour));
+					if (hoursDiff > interval)
 					{
-						taxManager.payTax(name);
+						// The difference in hours is greater than the interval
+						// Do the number of iterations of the specified tax
+						final double iterations = hoursDiff / interval;
+						for (int i = 0; i < iterations; i++)
+						{
+							taxManager.payTax(name);
+						}
+						delay = (long) (((hoursDiff % interval) * tickToHour) - (config.catchUpDelay * tickToHour));
 					}
+					if (delay < 0)
+					{
+						delay = 0;
+					}
+					tax.reschedule(delay, period);
 				}
-				//Set delay time
-				final long period = Math.round(interval * tickToHour) + 1;
-				long delay = (long) ((hoursDiff % interval) - (config.catchUpDelay * tickToHour));
-				if(delay < 0)
-				{
-					delay = 0;
-				}
-				tax.reschedule(delay, period);
 			}
-			//Grab all enabled salaries and check their time
-			for(Taxer salary : salaryManager.salaryTasks)
+			// Grab all enabled salaries and check their time
+			for (Taxer salary : salaryManager.salaryTasks)
 			{
 				final String name = salary.getName();
-				final long past = config.getLong("salaries." + name + ".lastpaid");
-				double hoursDiff = ((currentTime - past)*millisecondToSecond)/hoursToSeconds;
-				final double interval = config.getDouble(("salaries." + name + ".taxInterval"), 1);
-				if(hoursDiff > interval)
+				final long past = config.getLong("salaries." + name + ".lastPaid");
+				if (past > 0)
 				{
-					//The difference in hours is greater than the interval
-					//Do the number of iterations of the specified tax
-					final double iterations = hoursDiff / interval;
-					for(int i = 0; i < iterations; i++)
+					double hoursDiff = (((currentTime - past) * millisecondToSecond) / hoursToSeconds);
+					final double interval = config.getDouble(
+							("salaries." + name + ".salaryInterval"), 1);
+					final long period = Math.round(interval * tickToHour) + 1;
+					long delay = (long) ((interval * tickToHour) - (config.catchUpDelay * tickToHour));
+					if (hoursDiff > interval)
 					{
-						salaryManager.paySalary(name);
+						// The difference in hours is greater than the interval
+						// Do the number of iterations of the specified tax
+						final double iterations = hoursDiff / interval;
+						for (int i = 0; i < iterations; i++)
+						{
+							taxManager.payTax(name);
+						}
+						delay = (long) (((hoursDiff % interval) * tickToHour) - (config.catchUpDelay * tickToHour));
 					}
+					if (delay < 0)
+					{
+						delay = 0;
+					}
+					salary.reschedule(delay, period);
 				}
-				// Set delay time
-				final long period = Math.round(interval * tickToHour) + 1;
-				long delay = (long) ((hoursDiff % interval) - (config.catchUpDelay * tickToHour));
-				if(delay < 0)
-				{
-					delay = 0;
-				}
-				salary.reschedule(delay, period);
 			}
 			log.info(prefix + " Buffered iterations + Rescheduled threads");
 		}
